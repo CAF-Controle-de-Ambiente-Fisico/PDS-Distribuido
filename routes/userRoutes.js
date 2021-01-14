064bfc4b44f9bc6223002f963c2f90e7e2ea3c4d
vithorluc <vithorvarela.academico@gmail.com>
2021-01-14 14:56:17 -0300

    fix babel lib

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const UserController = require('./controllers/userController');
+// const UserController = require('./controllers/userController');
 
-router.get('/user', UserController.getUser());
+// router.get('/user', UserController.getUser());
 
-router.get('/user', UserController.getUser());
+// router.get('/user', UserController.getUser());
 
-router.get('/user', UserController.getUser());
+// router.get('/user', UserController.getUser());
 
-router.get('/user', UserController.getUser());
+// router.get('/user', UserController.getUser());
 
